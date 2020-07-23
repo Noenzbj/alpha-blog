1ad2eb7f43d2c749472de797a65070e23ad2e83a
Artur Abramow <arturabramow@gmx.de>
2020-07-23 13:48:02 +0200

    Error Msg added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
   get 'index', to: 'pages#index'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 
 end
