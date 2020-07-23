e9dfb545eecb73bd0ce09500b4b40ea2ed7a72d9
Artur Abramow <arturabramow@gmx.de>
2020-07-23 11:07:47 +0200

    Add Articles List

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
   get 'index', to: 'pages#index'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 
 end
