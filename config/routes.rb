5ff373c2e1e8ef0cccce70e3f7755d3620a2bef4
Noenzbj <arturabramow@gmx.de>
2020-09-03 13:32:58 +0200

    something wrong happened

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'about', to: 'pages#about'
+  get 'index', to: 'pages#index'
+  get 'signup', to: 'users#new'
+  resources :articles
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
 end
